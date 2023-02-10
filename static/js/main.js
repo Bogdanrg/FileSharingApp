ee0521429e13099e4dcb0351af7d6f9065da9b5f
Bogdanrg <70534359+Bogdanrg@users.noreply.github.com>
2023-02-10 17:37:33 +0300

    Delete static directory

--- static/js/main.js
+++
@@ -1,9 +0,0 @@
-function copy_clipboard(el) {
-
-  // Copy the text inside the text field
-  navigator.clipboard.writeText(el.innerHTML);
-  let is_copied = document.getElementById("#iscopied");
-  console.log(is_copied);
-  is_copied.innerHTML = "copied";
-  is_copied.style.opacity = "50%";
-}
